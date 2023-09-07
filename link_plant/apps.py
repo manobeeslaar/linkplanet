47e2139cee3b2f0de2b9f5fde2b0513c79ee23c5
Mano <mano.beeslaar@gmail.com>
2023-09-07 08:04:40 +0200

    added-edit-delete-views

--- link_plant/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LinkPlantConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "link_plant"
--- link_plant/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import LinkListView, LinkCreateView
-
-urlpatterns = [
-    path("", LinkListView.as_view(), name="link_list"),
-    path("link/create/", LinkCreateView.as_view(), name="link_create"),
-]
